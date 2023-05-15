5f5be1c419dceb8826b92a867887b1c0d348fee2
Shyam <shyamenk@gmail.com>
2023-05-15 21:37:35 +0530

    ddw

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,17 +1,17 @@
 import { SignIn } from '@clerk/nextjs/app-beta'
 
-// interface PageProps {
-//   searchParams: {
-//     redirectUrl?: string
-//   }
-// }
-export default function Page() {
-  // const { redirectUrl } = searchParams
+interface PageProps {
+  searchParams: {
+    redirectUrl?: string
+  }
+}
+export default function Page({ searchParams }: PageProps) {
+  const { redirectUrl } = searchParams
   return (
     <section className="py-24">
       <div className="container">
         <div className="flex justify-center">
-          <SignIn />
+          <SignIn redirectUrl={redirectUrl || '/'} />
         </div>
       </div>
     </section>
